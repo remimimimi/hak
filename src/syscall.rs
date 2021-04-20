f59bf4608c1e6c3e32539d339b2c98508455d42e
rrremiii <valent.xarin@gmail.com>
2021-04-20 20:42:26 +0300

    Add documentation

--- src/syscall.rs
+++ src/syscall.rs
@@ -1,3 +1,53 @@
+//! # Syscall
+//! We should implement some system calls for compatability with libgloss and
+//! respectivly with newlib. So there's list of libgloss syscall
+//! ```
+//! // These system call numbers come from libgloss so that we can use newlib
+//! // for our system calls.
+//! // Libgloss wants the system call number in A7 and arguments in A0..A6
+//! #define SYS_getcwd 17
+//! #define SYS_dup 23
+//! #define SYS_fcntl 25
+//! #define SYS_faccessat 48
+//! #define SYS_chdir 49
+//! #define SYS_openat 56
+//! #define SYS_close 57
+//! #define SYS_getdents 61
+//! #define SYS_lseek 62
+//! #define SYS_read 63
+//! #define SYS_write 64
+//! #define SYS_writev 66
+//! #define SYS_pread 67
+//! #define SYS_pwrite 68
+//! #define SYS_fstatat 79
+//! #define SYS_fstat 80
+//! #define SYS_exit 93
+//! #define SYS_exit_group 94
+//! #define SYS_kill 129
+//! #define SYS_rt_sigaction 134
+//! #define SYS_times 153
+//! #define SYS_uname 160
+//! #define SYS_gettimeofday 169
+//! #define SYS_getpid 172
+//! #define SYS_getuid 174
+//! #define SYS_geteuid 175
+//! #define SYS_getgid 176
+//! #define SYS_getegid 177
+//! #define SYS_brk 214
+//! #define SYS_munmap 215
+//! #define SYS_mremap 216
+//! #define SYS_mmap 222
+//! #define SYS_open 1024
+//! #define SYS_link 1025
+//! #define SYS_unlink 1026
+//! #define SYS_mkdir 1030
+//! #define SYS_access 1033
+//! #define SYS_stat 1038
+//! #define SYS_lstat 1039
+//! #define SYS_time 1062
+//! #define SYS_getmainvars 2011
+//! ```
+
 use alloc::{
     boxed::Box,
     string::String,
@@ -98,6 +148,8 @@ impl From<Syscall> for usize {
     }
 }
 
+/// System calls handler
+///
 /// [`do_syscall`], is called from trap.rs to invoke a system call. No discernment is
 /// made here whether this is a U-mode, S-mode, or M-mode system call.
 /// Since we can't do anything unless we dereference the passed pointer,
@@ -328,6 +380,8 @@ pub unsafe fn do_syscall(mepc: usize, frame: *mut TrapFrame) -> usize {
     )
 }
 
+/// Extern assembly function that correlates registers to proper
+/// for abi compatability
 extern "C" {
     fn make_syscall(
         syscall_number: usize,
@@ -340,6 +394,7 @@ extern "C" {
     ) -> usize;
 }
 
+/// Wrapper over [`make_syscall`] to reduce [`unsafe`] blocks usage
 fn do_make_syscall(
     syscall_number: usize,
     arg0: usize,
@@ -352,14 +407,17 @@ fn do_make_syscall(
     unsafe { make_syscall(syscall_number, arg0, arg1, arg2, arg3, arg4, arg5) }
 }
 
+/// Close current process
 pub fn syscall_exit() {
     let _ = do_make_syscall(Syscall::_Exit as usize, 0, 0, 0, 0, 0, 0);
 }
 
+/// Overlay Calling Process and Run New Program
 pub fn syscall_execv(path: *const u8, argv: usize) -> usize {
     do_make_syscall(Syscall::Execv.into(), path as usize, argv, 0, 0, 0, 0)
 }
 
+/// Read file in file system
 pub fn syscall_fs_read(dev: usize, inode: u32, buffer: *mut u8, size: u32, offset: u32) -> usize {
     do_make_syscall(
         Syscall::Read.into(),
@@ -372,6 +430,7 @@ pub fn syscall_fs_read(dev: usize, inode: u32, buffer: *mut u8, size: u32, offse
     )
 }
 
+/// Read the block on device
 pub fn syscall_block_read(dev: usize, buffer: *mut u8, size: u32, offset: u32) -> u8 {
     do_make_syscall(
         Syscall::BlockRead.into(),
@@ -384,10 +443,14 @@ pub fn syscall_block_read(dev: usize, buffer: *mut u8, size: u32, offset: u32) -
     ) as u8
 }
 
+/// Gives a little sleep to the process
+///
+/// He worked so hard!
 pub fn syscall_sleep(duration: usize) {
     let _ = do_make_syscall(Syscall::Sleep.into(), duration, 0, 0, 0, 0, 0);
 }
 
+/// Get process id
 pub fn syscall_get_pid() -> u16 {
     do_make_syscall(Syscall::GetPid.into(), 0, 0, 0, 0, 0, 0) as u16
 }
@@ -420,47 +483,3 @@ fn exec_func(args: usize) {
         }
     }
 }
-// These system call numbers come from libgloss so that we can use newlib
-// for our system calls.
-// Libgloss wants the system call number in A7 and arguments in A0..A6
-// #define SYS_getcwd 17
-// #define SYS_dup 23
-// #define SYS_fcntl 25
-// #define SYS_faccessat 48
-// #define SYS_chdir 49
-// #define SYS_openat 56
-// #define SYS_close 57
-// #define SYS_getdents 61
-// #define SYS_lseek 62
-// #define SYS_read 63
-// #define SYS_write 64
-// #define SYS_writev 66
-// #define SYS_pread 67
-// #define SYS_pwrite 68
-// #define SYS_fstatat 79
-// #define SYS_fstat 80
-// #define SYS_exit 93
-// #define SYS_exit_group 94
-// #define SYS_kill 129
-// #define SYS_rt_sigaction 134
-// #define SYS_times 153
-// #define SYS_uname 160
-// #define SYS_gettimeofday 169
-// #define SYS_getpid 172
-// #define SYS_getuid 174
-// #define SYS_geteuid 175
-// #define SYS_getgid 176
-// #define SYS_getegid 177
-// #define SYS_brk 214
-// #define SYS_munmap 215
-// #define SYS_mremap 216
-// #define SYS_mmap 222
-// #define SYS_open 1024
-// #define SYS_link 1025
-// #define SYS_unlink 1026
-// #define SYS_mkdir 1030
-// #define SYS_access 1033
-// #define SYS_stat 1038
-// #define SYS_lstat 1039
-// #define SYS_time 1062
-// #define SYS_getmainvars 2011
